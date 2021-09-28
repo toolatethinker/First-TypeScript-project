9421eccd17aa244a1811ae0516b6422aa617fbb4
Raghav <ronnie20028@gmail.com>
2021-09-28 18:47:16 +0530

    Fist typeScript Project.

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,14 @@
+import { HasFormatter } from "../Interface/HasFormatter.js";
+
+export class Invoice implements HasFormatter{
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ){}
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+
+}
